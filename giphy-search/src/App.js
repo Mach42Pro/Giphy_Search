0245dd65028c3ed6fb762b7b50ff61dbabf8cc7c
ryansmane <ryanzimmer8@gmail.com>
2020-05-07 14:30:28 -0400

    done with bonuses

---
+++ giphy-search/src/App.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import {errorStyle, noErrorStyle} from './styleOptions'
+import './App.css';
+const axios = require('axios');
+
+function App() {
+   const key = 'McEqCYL7IllxFGsn8dCGCpiLlRktypOB';
+   const [searchQuery, setSearchQuery] = useState('');
+   const [resultsArray, setResultsArray] = useState();
+   const [errorFlag, setErrorFlag] = useState();
+   const [currentOffset, setCurrentOffset] = useState();
+   const [showFlag, setShowFlag] = useState(false);
+
+  useEffect(() => {
+    setErrorFlag(false);
+    const fetch = async () => {
+      const results = await axios.get(
+         `http://api.giphy.com/v1/gifs/search?q=${searchQuery}&api_key=${key}&limit=5&offset=${currentOffset}`
+      );
+      setResultsArray(results.data.data);
+    }
+
+    fetch();
+          
+  }, [currentOffset])
+
+  const fetchGifs = (e, str = false) => {
+      e.preventDefault();
+      setShowFlag(true);
+      if (searchQuery === '') {
+        setErrorFlag(true);
+      } else {
+          switch (str) {
+            case 'next':
+              setCurrentOffset(currentOffset+5);
+              break;
+            case 'previous':
+              setCurrentOffset(Math.max(0, currentOffset-5));
+              break;
+            default:
+              setCurrentOffset(0);
+              
+          }
+      }
+    }
+
+   return (
+      <div>
+         <div className='search-container'>
+            <form className='search-form'>
+               <input
+                  type='text'
+                  style={errorFlag ? errorStyle : noErrorStyle}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+               ></input>
+               <button className='input-button'
+                  onClick={(e) => {
+                     fetchGifs(e);
+                  }}
+               >
+                  Search
+               </button>
+            </form>
+            {errorFlag && <p>Search parameters cannot be empty.</p>}
+         </div>
+         {resultsArray && showFlag && <div className='gif-container'>
+            <button onClick={(e) => fetchGifs(e, 'previous')}>Previous 5</button>
+               {resultsArray.map((gif) => {
+                  return (
+                     <>
+                        <img
+                           className='gif'
+                           src={gif.images.original.url}
+                           alt={searchQuery}
+                        />
+                     </>
+                  );
+               })}
+            <button onClick={(e) => fetchGifs(e, 'next')}>Next 5</button>
+         </div>}
+      </div>
+   );
+}
+
+export default App;
---
+++ giphy-search/src/styleOptions.js
@@ -0,0 +1,13 @@
+export let errorStyle = {
+    border: '2px solid red',
+    height: '56px',
+    width: '172px',
+    outline: 'none'
+}
+
+export let noErrorStyle = {
+   border: '2px solid blue',
+   height: '56px',
+   width: '172px',
+   outline: 'none'
+};
