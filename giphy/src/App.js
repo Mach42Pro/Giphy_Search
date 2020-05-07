28dc19aef26225fed7b48cebb169198dbbcc9907
ngumina17 <nicole.gumina17@gmail.com>
2020-05-07 13:21:40 -0500

    submitting

--- giphy/src/App.js
+++ giphy/src/App.js
@@ -3,8 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import Search from "./components/search"
 
-
-
 class App extends Component {
   constructor(props) {
     super(props)
@@ -12,9 +10,9 @@ class App extends Component {
       gifs: {}
     }
   }
-   
+ 
   fetchGifs(searchText) {
-    fetch(`http://api.giphy.com/v1/gifs/random?&tag=${searchText}&api_key=${process.env.REACT_APP_GIPHY_API_KEY}`)
+    fetch(`http://api.giphy.com/v1/gifs/search?q=${searchText}&api_key=${process.env.REACT_APP_GIPHY_API_KEY}&limit=5`)
     .then(response => response.json())
     .catch(error => console.error('Error:', error))
     .then(json => this.setState({
@@ -24,6 +22,7 @@ class App extends Component {
 }
   componentDidMount() {
     this.fetchGifs()
+
 
   }
 
