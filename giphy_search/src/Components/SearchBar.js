52066f4d59295c245953d76598f759753fa6536f
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-05-07 16:57:16 -0400

    transfered all files

---
+++ giphy_search/src/Components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function SearchBar(props) {
+
+    return (
+        <div class="search-bar">
+            <form onSubmit={props.handleSubmit}>
+                <input onChange={props.handleChange} placeholder="Search Gifys"></input>
+                <button type="submit">search</button>
+            </form>
+        </div>
+    )
+}
+
+
+export default SearchBar;
